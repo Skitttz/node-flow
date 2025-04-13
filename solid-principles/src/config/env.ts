bbe9aa37c1d17915c634ccbd59eb107fabcb3fdb
SKitttz <skittz.dev@gmail.com>
2025-04-13 18:25:07 -0300

    chore(phase-3): init solid principles

---
+++ solid-principles/src/config/env.ts
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+
+const envSchema = z.object({
+  NODE_ENV:  z.enum(['development', 'test', 'production']).default('production'),
+  JWT_SECRET: z.string(),
+  PORT: z.coerce.number().default(3333),
+});
+
+export const _env = envSchema.safeParse(process.env)
+
+if(_env.success === false){
+  console.error('Invalid env variables', _env.error.format());
+  throw new Error('Invalid env variables.')
+}
+
+export const env = _env.data;
---
+++ solid-principles/src/server.ts
@@ -0,0 +1,9 @@
+import { app } from "./app"
+import { env } from "./config/env"
+
+
+app.listen({
+  host: ("RENDER" in process.env) ? '0.0.0.0' : 'localhost',
+  port: env.PORT
+}).then(() => {
+  console.log(`[PORT: ${env.PORT}] HTTP Server is Running! 🚀`)})
