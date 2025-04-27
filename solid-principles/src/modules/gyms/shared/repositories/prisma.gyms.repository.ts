4dab502e2fdf06323206c1e5392ec8ae06a89460
SKitttz <skittz.dev@gmail.com>
2025-04-27 20:51:55 -0300

    feat(phase-3): create factory prisma gyms

---
+++ solid-principles/src/modules/gyms/shared/repositories/prisma.gyms.repository.ts
@@ -0,0 +1,44 @@
+import { prisma } from "@@app/lib/prisma";
+import { Gym, Prisma } from "@prisma/client";
+import { GymsRepository, NearbyGymsParams } from "./gym.repository.interface";
+
+export class PrismaGymsRepository implements GymsRepository {
+  async create(data: Prisma.GymCreateInput){
+    const gym = await prisma.gym.create({
+      data,
+    })
+
+    return gym;
+  }
+
+  async findNearByGyms({latitude,longitude}: NearbyGymsParams){
+    const gyms = await prisma.$queryRaw<Gym[]>`
+    SELECT * from gyms
+    WHERE ( 6371 * acos( cos( radians(${latitude}) ) * cos( radians( latitude ) ) * cos( radians( longitude ) - radians(${longitude}) ) + sin( radians(${latitude}) ) * sin( radians( latitude ) ) ) ) <= 10
+    `
+
+    return gyms;
+  }
+
+  async searchManyGyms(searchTerm: string, page:number){
+    const gyms = await prisma.gym.findMany({
+      where:{
+        title: {contains: searchTerm}
+      },
+      take: 20,
+      skip: (page - 1) * 20
+    })
+    return gyms;
+  }
+
+  async findGymById(gymId: string){
+    const gym = await prisma.gym.findUnique({
+      where:{
+        id: gymId
+      }
+    })
+
+    return gym;
+  }
+  
+}
