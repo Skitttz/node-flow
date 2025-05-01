ead5313b1045f5cd9df0dac7b0b36d7c4a5e6565
SKitttz <skittz.dev@gmail.com>
2025-04-30 23:53:22 -0300

    refact(phase-3): add new routes in App

--- solid-principles/src/shared/routes.ts
+++ solid-principles/src/shared/routes.ts
@@ -1,8 +1,15 @@
-enum AppRoutesEnum{
-  USER = '/users',
-  SESSION = '/sessions',
-  PROFILE = '/me'
+enum AppRoutesEnum {
+  USER = "/users",
+  SESSION = "/sessions",
+  REFRESH_TOKEN = '/token/refresh',
+  PROFILE = "/me",
+  GYM = "/gyms",
+  GYM_SEARCH = "/gyms/search",
+  GYM_NEARBY = "/gyms/nearby",
+  CHECK_IN = "/gyms/:gymId/check-ins",
+  CHECK_IN_HISTORY = "/check-ins/history",
+  CHECK_IN_VALIDATE = "/check-ins/:checkInId/validate",
+  CHECK_IN_METRIC = "/check-ins/metrics",
 }
-
 
 export { AppRoutesEnum };
